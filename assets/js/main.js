cba642aa7375df46cecae22d4b66bda1d852868c
HoTuHi <hotuhi12@gmail.com>
2021-07-05 16:18:49 +0700

    4/7/2021 Dear Hi, that another day, good job !!! update app details popup 🌹🎉🎉🌹✌🎉🌹🌹💋🎉

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,271 +1,292 @@
-(function ($)
-  { "use strict"
-  
-/* 1. Proloder */
+(function ($) {
+    "use strict"
+
+    /* 1. Proloder */
     $(window).on('load', function () {
-      $('#preloader-active').delay(450).fadeOut('slow');
-      $('body').delay(450).css({
-        'overflow': 'visible'
-      });
+        $('#preloader-active').delay(450).fadeOut('slow');
+        $('body').delay(450).css({
+            'overflow': 'visible'
+        });
     });
 
 
-/* 2. slick Nav */
+    /* 2. slick Nav */
 // mobile_menu
     var menu = $('ul#navigation');
-    if(menu.length){
-      menu.slicknav({
-        prependTo: ".mobile_menu",
-        closedSymbol: '+',
-        openedSymbol:'-'
-      });
-    };
+    if (menu.length) {
+        menu.slicknav({
+            prependTo: ".mobile_menu",
+            closedSymbol: '+',
+            openedSymbol: '-'
+        });
+    }
+    ;
 
 
-/* 3. MainSlider-1 */
+    /* 3. MainSlider-1 */
+
     // h1-hero-active
     function mainSlider() {
-      var BasicSlider = $('.slider-active');
-      BasicSlider.on('init', function (e, slick) {
-        var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
-        doAnimations($firstAnimatingElements);
-      });
-      BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-        var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
-        doAnimations($animatingElements);
-      });
-      BasicSlider.slick({
-        autoplay: false,
-        autoplaySpeed: 4000,
-        dots: false,
-        fade: true,
-        arrows: false, 
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/hero_thumb/arrow-left.png" alt=""><img class="secondary-img" src="img/hero_thumb/left-white.png" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/hero_thumb/arrow-right.png" alt=""><img class="secondary-img" src="img/hero_thumb/right-white.png" alt=""></button>',
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-            }
-          },
-          {
-            breakpoint: 991,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows: false
-            }
-          },
-          {
-            breakpoint: 767,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows: false
-            }
-          }
-        ]
-      });
-
-      function doAnimations(elements) {
-        var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        elements.each(function () {
-          var $this = $(this);
-          var $animationDelay = $this.data('delay');
-          var $animationType = 'animated ' + $this.data('animation');
-          $this.css({
-            'animation-delay': $animationDelay,
-            '-webkit-animation-delay': $animationDelay
-          });
-          $this.addClass($animationType).one(animationEndEvents, function () {
-            $this.removeClass($animationType);
-          });
+        var BasicSlider = $('.slider-active');
+        BasicSlider.on('init', function (e, slick) {
+            var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+            doAnimations($firstAnimatingElements);
         });
-      }
+        BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+            var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+            doAnimations($animatingElements);
+        });
+        BasicSlider.slick({
+            autoplay: false,
+            autoplaySpeed: 4000,
+            dots: false,
+            fade: true,
+            arrows: false,
+            prevArrow: '<button type="button" class="slick-prev"><img src="img/hero_thumb/arrow-left.png" alt=""><img class="secondary-img" src="img/hero_thumb/left-white.png" alt=""></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="img/hero_thumb/arrow-right.png" alt=""><img class="secondary-img" src="img/hero_thumb/right-white.png" alt=""></button>',
+            responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+                {
+                    breakpoint: 991,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false
+                    }
+                }
+            ]
+        });
+
+        function doAnimations(elements) {
+            var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            elements.each(function () {
+                var $this = $(this);
+                var $animationDelay = $this.data('delay');
+                var $animationType = 'animated ' + $this.data('animation');
+                $this.css({
+                    'animation-delay': $animationDelay,
+                    '-webkit-animation-delay': $animationDelay
+                });
+                $this.addClass($animationType).one(animationEndEvents, function () {
+                    $this.removeClass($animationType);
+                });
+            });
+        }
     }
+
     mainSlider();
 
-/* 4. Testimonial Active*/
-  var testimonial = $('.h1-testimonial-active');
-    if(testimonial.length){
-    testimonial.slick({
-        dots: false,
-        infinite: true,
-        speed: 1000,
-        autoplay:false,
-        arrows: true,
-        prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: false,
-              arrow:false
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows:false
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows:false,
-            }
-          }
-        ]
-      });
+    /* 4. Testimonial Active*/
+    var testimonial = $('.h1-testimonial-active');
+    if (testimonial.length) {
+        testimonial.slick({
+            dots: false,
+            infinite: true,
+            speed: 1000,
+            autoplay: false,
+            arrows: true,
+            prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
+            nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: false,
+                        arrow: false
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                    }
+                }
+            ]
+        });
     }
 
 
-/* 5. Gallery Active */
+    /* 5. Gallery Active */
     var client_list = $('.gallery-active');
-    if(client_list.length){
-      client_list.owlCarousel({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        loop: true,
-        autoplay:true,
-        speed: 3000,
-        smartSpeed:2000,
-        nav: false,
-        dots: false,
-        margin: 0,
+    if (client_list.length) {
+        client_list.owlCarousel({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            loop: true,
+            autoplay: true,
+            speed: 3000,
+            smartSpeed: 2000,
+            nav: false,
+            dots: false,
+            margin: 0,
 
-        autoplayHoverPause: true,
-        responsive : {
-          0 : {
-            nav: false,
-            items: 2,
-          },
-          768 : {
-            nav: false,
-            items: 3,
-          }
+            autoplayHoverPause: true,
+            responsive: {
+                0: {
+                    nav: false,
+                    items: 2,
+                },
+                768: {
+                    nav: false,
+                    items: 3,
+                }
+            }
+        });
+    }
+
+
+    /* 6. Nice Selectorp  */
+    var nice_Select = $('select');
+    if (nice_Select.length) {
+        nice_Select.niceSelect();
+    }
+
+    /* 7.  Custom Sticky Menu  */
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll < 245) {
+            $(".header-sticky").removeClass("sticky-bar");
+        } else {
+            $(".header-sticky").addClass("sticky-bar");
         }
-      });
-    }
-
-
-/* 6. Nice Selectorp  */
-  var nice_Select = $('select');
-    if(nice_Select.length){
-      nice_Select.niceSelect();
-    }
-
-/* 7.  Custom Sticky Menu  */
-    $(window).on('scroll', function () {
-      var scroll = $(window).scrollTop();
-      if (scroll < 245) {
-        $(".header-sticky").removeClass("sticky-bar");
-      } else {
-        $(".header-sticky").addClass("sticky-bar");
-      }
     });
 
     $(window).on('scroll', function () {
-      var scroll = $(window).scrollTop();
-      if (scroll < 245) {
-          $(".header-sticky").removeClass("sticky");
-      } else {
-          $(".header-sticky").addClass("sticky");
-      }
+        var scroll = $(window).scrollTop();
+        if (scroll < 245) {
+            $(".header-sticky").removeClass("sticky");
+        } else {
+            $(".header-sticky").addClass("sticky");
+        }
     });
 
 
-
-/* 8. sildeBar scroll */
+    /* 8. sildeBar scroll */
     $.scrollUp({
-      scrollName: 'scrollUp', // Element ID
-      topDistance: '300', // Distance from top before showing element (px)
-      topSpeed: 300, // Speed back to top (ms)
-      animation: 'fade', // Fade, slide, none
-      animationInSpeed: 200, // Animation in speed (ms)
-      animationOutSpeed: 200, // Animation out speed (ms)
-      scrollText: '<i class="ti-arrow-up"></i>', // Text for element
-      activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+        scrollName: 'scrollUp', // Element ID
+        topDistance: '300', // Distance from top before showing element (px)
+        topSpeed: 300, // Speed back to top (ms)
+        animation: 'fade', // Fade, slide, none
+        animationInSpeed: 200, // Animation in speed (ms)
+        animationOutSpeed: 200, // Animation out speed (ms)
+        scrollText: '<i class="ti-arrow-up"></i>', // Text for element
+        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
     });
 
 
-/* 9. data-background */
+    /* 9. data-background */
     $("[data-background]").each(function () {
-      $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
-      });
+        $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
+    });
 
 
-/* 10. WOW active */
+    /* 10. WOW active */
     new WOW().init();
 
 
+    /* 13. counterUp*/
+    $('.counter').counterUp({
+        delay: 10,
+        time: 3000
+    });
 
-/* 13. counterUp*/
-  $('.counter').counterUp({
-    delay: 10,
-    time: 3000
-  });
 
-
-    
-// 11. ---- Mailchimp js --------//  
+// 11. ---- Mailchimp js --------//
     function mailChimp() {
-      $('#mc_embed_signup').find('form').ajaxChimp();
+        $('#mc_embed_signup').find('form').ajaxChimp();
     }
-    mailChimp();
 
+    mailChimp();
 
 
 // 12 Pop Up Img
     var popUp = $('.single_gallery_part, .img-pop-up');
-      if(popUp.length){
+    if (popUp.length) {
         popUp.magnificPopup({
-          type: 'image',
-          gallery:{
-            enabled:true
-          }
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
         });
-      }
-
-
+    }
 
 
 })(jQuery);
 
 // Get the modal
+var modal0 = document.getElementById("myModal0");
 var modal = document.getElementById("myModal");
-
+var modal1 = document.getElementById("myModal1");
+var modal3 = document.getElementById("myModal3");
 // Get the button that opens the modal
+var btn0 = document.getElementById("myBtn0");
 var btn = document.getElementById("myBtn");
-
+var btn1 = document.getElementById("myBtn1");
+var btn3 = document.getElementById("myBtn3");
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
+var span0 = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[2];
+var span1 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[3];
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btn0.onclick = function () {
+    modal0.style.display = "block";
 }
-
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+btn1.onclick = function () {
+    modal1.style.display = "block";
+}
+btn3.onclick = function () {
+    modal3.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span0.onclick = function () {
+    modal0.style.display = "none";
+}
+span.onclick = function () {
+    modal.style.display = "none";
+}
+span1.onclick = function () {
+    modal1.style.display = "none";
 }
 
+span3.onclick = function () {
+    modal3.style.display = "none";
+}
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
